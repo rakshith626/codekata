aab04513813fb05b1c896f9711056fac884e6ae8
Rakshith guptha <40866227+rakshith626@users.noreply.github.com>
2019-07-13 15:56:08 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -6,6 +6,6 @@ elif(c=='B' or c=='b' or c=='C' or c=='c' or c=='D'
  or c=='k' or c=='L' or c=='l' or c=='M' or c=='m' or c=='N' or c=='n' or c=='P' or c=='p' or c=='Q'
  or c=='q' or c=='R' or c=='r' or c=='S' or c=='s' or c=='T' or c=='t' or c=='V' or c=='v' or c=='W'
  or c=='w' or c=='x' or c=='X' or c=='Y' or c=='y' or c=='z' or c=='Z'):
-    print("Consonent")
+    print("Consonant")
 else:
     print("invalid")
