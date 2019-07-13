23b5e1405b1d0dbd9c1cace9b44037873d92fd08
Rakshith guptha <40866227+rakshith626@users.noreply.github.com>
2019-07-13 15:54:19 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,11 @@
+c=input()
+if(c=='a' or c=='e' or c=='i' or c=='o' or c=='u' or c=='A' or c=='E' or c=='I' or c=='O' or c=='U'):
+    print("Vowel")
+elif(c=='B' or c=='b' or c=='C' or c=='c' or c=='D'
+ or c=='d' or c=='F' or c=='f' or c=='g' or c=='G' or c=='H' or c=='h' or c=='j' or c=='J' or c=='K'
+ or c=='k' or c=='L' or c=='l' or c=='M' or c=='m' or c=='N' or c=='n' or c=='P' or c=='p' or c=='Q'
+ or c=='q' or c=='R' or c=='r' or c=='S' or c=='s' or c=='T' or c=='t' or c=='V' or c=='v' or c=='W'
+ or c=='w' or c=='x' or c=='X' or c=='Y' or c=='y' or c=='z' or c=='Z'):
+    print("Consonent")
+else:
+    print("Invalid")
